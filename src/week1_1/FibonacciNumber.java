0f73015aef5823f016b3430778654be88838f74e
chenghaowang <chenghaowangbc400@gmail.com>
2020-06-29 18:13:52 -0400

    Correct solution

--- src/week1_1/FibonacciNumber.java
+++ src/week1_1/FibonacciNumber.java
@@ -10,18 +10,15 @@ public class FibonacciNumber {
             int n = scanner.nextInt();
             if (n < 1)
                 break;
-            int answer = fib(n, 0,0,1);
+            int answer = fib(n, 1, 0, 1);
             System.out.println("The " + n + "th Fibonacci number is: " + answer);
         }
     }
 
     public static int fib(int n, int k, int f0, int f1) {
-        if (n != 0)
-            for (int i = 0; i < n - 1; i++) {
-                k = f0 + f1;
-                f1 = f0;
-                f0 = k;
-            }
-        return k;
+        if (n == k)
+            return f0;
+        else
+            return fib(n, k + 1, f1, f0 + f1);
     }
 }
